8a4aca9fedbbb9e71f18af1c7216ba1a5c43a043
Sofía Fernández <soffiafdz@Lazarus.>
2025-08-31 14:48:14 -0400

    BF: redundancy and table bug

--- nvim/.config/nvim/lua/plugins/r-nvim.lua
+++ nvim/.config/nvim/lua/plugins/r-nvim.lua
@@ -2,23 +2,17 @@
 return {
   "R-nvim/R.nvim",
   opts = function(_, opts)
-    -- Detect environment
-    local tmux = os.getenv("TMUX") ~= nil
-
-    opts = opts or {}
-    opts.view_df = opts.view_df or {}
-
-    -- Use radian
-    opts.R_app = "radian"
+    opts.R_app = "radian" -- Use radian
 
     -- Open data.frames with vd
-    opts.view_df.open_app = opts.view_df.open_app or "terminal:vd"
+    table.insert(opts.view_df, { open_app = "terminal:vd" })
 
-    -- Conditional adjustments
+    -- Conditional adjustments for TMUX
+    local tmux = os.getenv("TMUX") ~= nil
     if tmux then
       opts.config_tmux = true
       opts.external_term = "tmux split-window -hf"
-      opts.view_df.open_app = "tmux new-window 80 vd"
+      table.insert(opts.view_df, { open_app = "tmux new-window 80 vd" })
     end
 
     return opts
