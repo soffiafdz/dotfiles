5942fb2c49a76455d3dd65f7b5b7a3adc386bc2d
Sofía Fernández <soffiafdz@Lazarus.>
2025-09-01 00:38:48 -0400

    Ltex+ngrams (untested)

---
+++ nvim/.config/nvim/lua/plugins/ltex-ls.lua
@@ -0,0 +1,42 @@
+-- ltex.lua integration
+--[[
+To use different a language in Markdown,
+it is necessary to use magic comments: 
+  <!-- LTeX: language-es -->
+  <!-- LTeX: language-en-CA -->
+  <!-- LTeX: language-fr -->
+--]]
+-- Conditionally load ngrams
+local ngrams_path = vim.fn.expand("~/.local/share/nvim/models/ngrams/")
+local additionalRules
+if vim.fn.isdirectory(ngrams_path) == 1 then
+  additionalRules = {
+    motherTonge = "es",
+    languageModel = ngrams_path,
+  }
+else
+  additionalRules = { motherTonge = "es" }
+end
+
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "ltex" },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ltex = {
+          filetypes = { "markdown", "tex", "text" },
+          settings = {
+            language = "en-CA",
+            additionalRules = additionalRules,
+          },
+        },
+      },
+    },
+  },
+}
