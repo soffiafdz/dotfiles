29f2208e8f0ef34936cb4b0e2ffae9abd6b94d56
Sofia Fernandez <sofia.fernandezlozano@mail.mcgill.ca>
2025-08-31 20:59:05 -0400

    Setup vimwiki (untested) and laid groundwork for palimpsest setup

---
+++ nvim/.config/nvim/lua/plugins/palimpsest.lua
@@ -0,0 +1,16 @@
+-- Local Palimpsest project integration
+local palimpsest_path = vim.fn.expand("~/Documents/palimpsest")
+if vim.fn.isdirectory(palimpsest_path .. "/nvim") == 1 then
+  return {
+    {
+      "palimpsest/local",
+      name = "palimpsest.nvim",
+      dir = palimpsest_path .. "/nvim",
+      config = function()
+        require("palimpsest").setup()
+      end,
+    },
+  }
+else
+  return {}
+end
