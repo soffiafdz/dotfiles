0735930c7afb60f06e49acbdff9681c1021cb7e3
Sofia Fernandez <sofia.fernandezlozano@mail.mcgill.ca>
2025-08-31 20:37:22 -0400

    BF: changed insert.table function

--- nvim/.config/nvim/lua/plugins/r-nvim.lua
+++ nvim/.config/nvim/lua/plugins/r-nvim.lua
@@ -5,14 +5,14 @@ return {
     opts.R_app = "radian" -- Use radian
 
     -- Open data.frames with vd
-    table.insert(opts.view_df, { open_app = "terminal:vd" })
+    opts.view_df = { open_app = "terminal:vd" }
 
     -- Conditional adjustments for TMUX
     local tmux = os.getenv("TMUX") ~= nil
     if tmux then
       opts.config_tmux = true
       opts.external_term = "tmux split-window -hf"
-      table.insert(opts.view_df, { open_app = "tmux new-window 80 vd" })
+      opts.view_df = { open_app = "tmux new-window 80 vd" }
     end
 
     return opts
