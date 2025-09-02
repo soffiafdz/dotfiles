5a34d0f0dee8caadd4e9b4d1543b2c68e6d48e8f
Sofía Fernández <soffiafdz@Lazarus.>
2025-09-02 19:18:42 -0400

    Vimwiki keymap group

--- nvim/.config/nvim/lua/plugins/vimwiki.lua
+++ nvim/.config/nvim/lua/plugins/vimwiki.lua
@@ -6,33 +6,48 @@ return {
     -- TODO: Make sure that local keymaps work correctly.
     keys = {
       { "<leader>v", group = "Vimwiki" },
+
       -- Global mapping (any filetype)
-      { "<leader>vw", "<Plug>VimwikiIndex", desc = "Index", mode = "n" },
-      -- Filetype-restricted mappings
+      {
+        "<leader>vw",
+        "<Plug>VimwikiIndex",
+        desc = "Vimwiki Index",
+      },
       {
         "<leader>vt",
         "<Plug>VimwikiTabIndex",
-        desc = "Tab Index",
-        mode = "n",
-        ft = { "vimwiki" },
+        desc = "Vimwiki Index (Tab)",
       },
       {
-        "<leader>vd",
+        "<leader>vl",
+        "<Plug>VimwikiDiaryIndex",
+        desc = "Vimwiki Log",
+      },
+
+      -- Filetype-restricted mappings
+      {
+        "<leader>vt",
         "<Plug>VimwikiMakeDiaryNote",
-        desc = "Diary Today",
-        mode = "n",
+        desc = "Vimwiki Log (Today)",
         ft = { "vimwiki" },
       },
       {
-        "<leader>vs",
+        "<leader>vr",
         "<Plug>VimwikiDiaryGenerateLinks",
-        desc = "Rebuild Links",
-        mode = "n",
+        desc = "Rebuild log links",
         ft = { "vimwiki" },
       },
     },
     config = function()
+      vim.g.vimwiki_key_mappings = {
+        global = 0,
+        table_format = 0,
+        table_mappings = 0,
+        html = 0,
+      }
       vim.g.vimwiki_global_ext = 0
+      vim.g.ext2syntax = {}
+      vim.g.vimwiki_markdown_link_ext = 1
     end,
   },
 }
