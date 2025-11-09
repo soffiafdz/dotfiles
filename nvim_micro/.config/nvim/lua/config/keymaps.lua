edd14c6e1ebf86c6e4fd8e784cef6222fba96b1f
Sofía Fernández <sofia.fernandezlozano@mail.mcgill.ca>
2025-11-09 15:18:30 -0500

    Keymaps for linters

--- nvim_micro/.config/nvim/lua/config/keymaps.lua
+++ nvim_micro/.config/nvim/lua/config/keymaps.lua
@@ -1,31 +1,62 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
--- Toggle ltex LSP on/off
-vim.keymap.set("n", "<leader>lg", function()
-  local clients = vim.lsp.get__clients({ name = "ltex" })
-  if #clients > 0 then
-    vim.cmd("LspStop ltex")
-    vim.notify("ltex stopped", vim.log.levels.INFO)
-  else
-    vim.cmd("LspStart ltex")
-    vim.notify("ltex started", vim.log.levels.INFO)
-  end
-end, { desc = "Toggle ltex grammar checking" })
+local linter_icon = ""
+local wk = require("which-key")
 
--- Manual lint trigger
-vim.keymap.set("n", "<leader>ll", function()
-  require("lint").try_lint()
-  vim.notify("Linting...", vim.log.levels.INFO)
-end, { desc = "Lint current file" })
+wk.add({
+  {
+    group = "Linter",
+    icon = { icon = "󱆨", color = "orange" }, -- or use a lint-related icon
+    { "<leader>l", group = "Linter" },
 
--- Lint with specific linter
-vim.keymap.set("n", "<leader>lv", function()
-  require("lint").try_lint("vale")
-  vim.notify("Running vale...", vim.log.levels.INFO)
-end, { desc = "Lint with vale" })
+    -- LSP grammar checking
+    {
+      "<leader>lg",
+      function()
+        local clients = vim.lsp.get_clients({ name = "ltex" })
+        if #clients > 0 then
+          vim.cmd("LspStop ltex")
+          vim.notify("ltex stopped", vim.log.levels.INFO)
+        else
+          vim.cmd("LspStart ltex")
+          vim.notify("ltex started", vim.log.levels.INFO)
+        end
+      end,
+      desc = "Toggle ltex grammar",
+      icon = "󰓆",
+    },
 
-vim.keymap.set("n", "<leader>lw", function()
-  require("lint").try_lint("write_good")
-  vim.notify("Running write-good...", vim.log.levels.INFO)
-end, { desc = "Lint with write-good" })
+    -- General linting
+    {
+      "<leader>ll",
+      function()
+        require("lint").try_lint()
+        vim.notify("Linting...", vim.log.levels.INFO)
+      end,
+      desc = "Lint current file",
+      icon = "󰁨",
+    },
+
+    -- Vale linter
+    {
+      "<leader>lv",
+      function()
+        require("lint").try_lint("vale")
+        vim.notify("Running vale...", vim.log.levels.INFO)
+      end,
+      desc = "Lint with vale",
+      icon = "󰓆",
+    },
+
+    {
+      "<leader>lw",
+      function()
+        require("lint").try_lint("write_good")
+        vim.notify("Running write-good...", vim.log.levels.INFO)
+      end,
+      desc = "Lint with write-good",
+      icon = "󰓆",
+    },
+  },
+})
