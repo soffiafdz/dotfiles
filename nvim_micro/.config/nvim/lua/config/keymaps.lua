680ee931de69b5eac577ba17d92e4de5092fa5b0
Sofía Fernández <sofia.fernandezlozano@mail.mcgill.ca>
2025-12-18 01:15:36 -0500

    ENH, update and upgrade

--- nvim_micro/.config/nvim/lua/config/keymaps.lua
+++ nvim_micro/.config/nvim/lua/config/keymaps.lua
@@ -1,61 +1,32 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
-local linter_icon = ""
 local wk = require("which-key")
 
 wk.add({
   {
-    group = "Linter",
-    icon = { icon = "󱆨", color = "orange" }, -- or use a lint-related icon
-    { "<leader>l", group = "Linter" },
+    group = "LSP/Grammar",
+    icon = { icon = "󱆨", color = "orange" },
+    { "<leader>l", group = "LSP/Grammar" },
 
     -- LSP grammar checking
     {
       "<leader>lg",
       function()
-        local clients = vim.lsp.get_clients({ name = "ltex" })
+        local clients = vim.lsp.get_clients({ name = "ltex_plus" })
         if #clients > 0 then
-          vim.cmd("LspStop ltex")
+          vim.cmd("LspStop ltex_plus")
+          -- Disable autostart for ltex
+          vim.g.ltex_enabled = false
           vim.notify("ltex stopped", vim.log.levels.INFO)
         else
-          vim.cmd("LspStart ltex")
+          -- Enable autostart for ltex
+          vim.g.ltex_enabled = true
+          vim.cmd("LspStart ltex_plus")
           vim.notify("ltex started", vim.log.levels.INFO)
         end
       end,
       desc = "Toggle ltex grammar",
-      icon = "󰓆",
-    },
-
-    -- General linting
-    {
-      "<leader>ll",
-      function()
-        require("lint").try_lint()
-        vim.notify("Linting...", vim.log.levels.INFO)
-      end,
-      desc = "Lint current file",
-      icon = "󰁨",
-    },
-
-    -- Vale linter
-    {
-      "<leader>lv",
-      function()
-        require("lint").try_lint("vale")
-        vim.notify("Running vale...", vim.log.levels.INFO)
-      end,
-      desc = "Lint with vale",
-      icon = "󰓆",
-    },
-
-    {
-      "<leader>lw",
-      function()
-        require("lint").try_lint("write_good")
-        vim.notify("Running write-good...", vim.log.levels.INFO)
-      end,
-      desc = "Lint with write-good",
       icon = "󰓆",
     },
   },
