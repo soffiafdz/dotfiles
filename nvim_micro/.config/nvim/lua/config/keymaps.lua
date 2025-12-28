8a553136eb8ad76f4d4a9fdb1a43c9867697532b
Sofía Fernández <sofia.fernandezlozano@mail.mcgill.ca>
2025-12-27 20:03:27 -0800

    Strip LSP from nvim_micro, update yazi image opener to sxiv

--- nvim_micro/.config/nvim/lua/config/keymaps.lua
+++ nvim_micro/.config/nvim/lua/config/keymaps.lua
@@ -1,33 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
-local wk = require("which-key")
-
-wk.add({
-  {
-    group = "LSP/Grammar",
-    icon = { icon = "󱆨", color = "orange" },
-    { "<leader>l", group = "LSP/Grammar" },
-
-    -- LSP grammar checking
-    {
-      "<leader>lg",
-      function()
-        local clients = vim.lsp.get_clients({ name = "ltex_plus" })
-        if #clients > 0 then
-          vim.cmd("LspStop ltex_plus")
-          -- Disable autostart for ltex
-          vim.g.ltex_enabled = false
-          vim.notify("ltex stopped", vim.log.levels.INFO)
-        else
-          -- Enable autostart for ltex
-          vim.g.ltex_enabled = true
-          vim.cmd("LspStart ltex_plus")
-          vim.notify("ltex started", vim.log.levels.INFO)
-        end
-      end,
-      desc = "Toggle ltex grammar",
-      icon = "󰓆",
-    },
-  },
-})
+-- No custom keymaps needed for nvim_micro
+-- All heavy features (linters, LSP grammar) are disabled for Pi Zero 2W
+-- Use Neovim's built-in spell checking instead: ]s, [s, z=, zg
