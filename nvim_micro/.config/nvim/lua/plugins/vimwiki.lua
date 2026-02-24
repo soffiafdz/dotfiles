27e525268294eedf21a2daece3c41646d2987484
Sofía Fernández <sofia.fernandezlozano@mail.mcgill.ca>
2026-02-23 20:42:17 -0500

    Clean up nvim_micro: remove dead yaml extra and stale vimwiki keymaps

--- nvim_micro/.config/nvim/lua/plugins/vimwiki.lua
+++ nvim_micro/.config/nvim/lua/plugins/vimwiki.lua
@@ -7,46 +7,6 @@ return {
     init = function()
       vim.g.vimwiki_map_prefix = "<leader>v"
     end,
-    -- TODO: Make sure that local keymaps work correctly.
-
-    -- keys = {
-    --   -- Global mapping (any filetype)
-    --   {
-    --     "<leader>vw",
-    --     "<Plug>VimwikiIndex",
-    --     desc = "Vimwiki Index",
-    --   },
-    --   {
-    --     "<leader>vt",
-    --     "<Plug>VimwikiTabIndex",
-    --     desc = "Vimwiki Index (New tab)",
-    --   },
-    --   {
-    --     "<leader>vl",
-    --     "<Plug>VimwikiDiaryIndex",
-    --     desc = "Vimwiki Log",
-    --   },
-    --
-    --   -- Filetype-restricted mappings
-    --   {
-    --     "<leader>v<leader>w",
-    --     "<Plug>VimwikiMakeDiaryNote",
-    --     desc = "Vimwiki Log (Today)",
-    --     ft = { "vimwiki" },
-    --   },
-    --   {
-    --     "<leader>v<leader>t",
-    --     "<Plug>VimwikiTabMakeDiaryNote",
-    --     desc = "Palimpsest Log (Today, new tab)",
-    --     ft = { "vimwiki" },
-    --   },
-    --   {
-    --     "<leader>vr",
-    --     "<Plug>VimwikiDiaryGenerateLinks",
-    --     desc = "Rebuild log links",
-    --     ft = { "vimwiki" },
-    --   },
-    -- },
     config = function()
       vim.g.vimwiki_key_mappings = {
         table_format = 0,
