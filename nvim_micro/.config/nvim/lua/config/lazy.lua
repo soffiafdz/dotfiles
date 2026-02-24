f02e2a9bde8442acb4499c0fdbb9949ba777c3f7
Sofía Fernández <sofia.fernandezlozano@mail.mcgill.ca>
2026-02-23 20:42:17 -0500

    Clean up nvim_micro: remove dead yaml extra and stale vimwiki keymaps

--- nvim_micro/.config/nvim/lua/config/lazy.lua
+++ nvim_micro/.config/nvim/lua/config/lazy.lua
@@ -24,7 +24,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-comment" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.lang.git" },
-    { import = "lazyvim.plugins.extras.lang.yaml" },
     -- Plugins
     { import = "plugins" },
   },
